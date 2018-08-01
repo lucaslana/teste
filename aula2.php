afc9c29e06f51838471039664de72af417331da2
lucas lana <lucas.lana@monetizze.com.br>
2018-08-01 17:16:56 -0300

    first commit

---
+++ aula2.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+   <title< Hello Word em PHP</title>
+   <meta charset="utf-8" />
+   
+   </head>
+
+<body>
+     <?php
+	    echo " Senha bem vindo ao PHP! Vamos quebrar a maldiçao do Hello World";
+	   ?>
+</body>
+</html>
